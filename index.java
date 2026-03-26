d5d67bbc6b7d70fabd865c252ee1b97437126ba5
Atul Gupta <atulgupta6910@gmail.com>
2026-03-26 16:03:27 +0530

    First commit

---
+++ index.java
@@ -0,0 +1,16 @@
+class Main{
+    public static void main(String [] args){
+
+        String name ="Atul Gupta";
+
+        String role ="";
+
+        
+
+        
+    }
+
+    public String getName(){
+        return "Atul";
+    }
+}
---
+++ index2.java
@@ -0,0 +1,3 @@
+class Index2{
+    
+}
